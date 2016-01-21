9659938326ee70af8449ae65fea248c948347062
hagne <hagen@hagnet.net>
2016-01-20 19:42:46 -0700

    3D plotting for dataframes and panels

--- atmPy/tools/pandas_tools.py
+++ atmPy/tools/pandas_tools.py
@@ -1,4 +1,5 @@
-
+import numpy as np
+import pylab as plt
 
 
 def ensure_column_exists(df, col_name, col_alt = False):
@@ -28,3 +29,66 @@ def ensure_column_exists(df, col_name, col_alt = False):
                 txt += 'Neither one of the alternatives: %s'(col_alt)
             raise AttributeError(txt)
     return
+
+def plot_dataframe_meshgrid(df, xaxis = 0):
+    axes_list = [df.index, df.columns]
+    x_index = axes_list[xaxis]
+
+    if xaxis !=0:
+        df = df.swapaxes(0,1)
+        y_index = axes_list[0]
+    else:
+        y_index = axes_list[1]
+
+    z = df.values.transpose()
+    x = np.repeat(np.array([x_index]), y_index.shape[0], axis = 0)
+    y = np.repeat(np.array([y_index]), x_index.shape[0], axis = 0).transpose()
+
+    f,a = plt.subplots()
+    pc = a.pcolormesh(x, y , z)
+
+
+    if 'datetime' in df.index.dtype_str:
+        f.autofmt_xdate()
+    cb = f.colorbar(pc)
+    a.set_xlabel(df.index.name)
+    a.set_ylabel(df.columns.name)
+    return f,a,pc,cb
+
+def plot_panel_meshgrid(panel, xaxis = 0, yaxis = 1, sub_set = 0, kwargs = {}):
+
+    valid_axes = np.array([0,1,2])
+    zaxis = valid_axes[np.logical_and(valid_axes != xaxis, valid_axes != yaxis)][0]
+    axes_list = [panel.items, panel.major_axis, panel.minor_axis]
+
+    axes_idx_list_tobe = np.array([int(xaxis),int(yaxis),int(zaxis)])
+    axes_idx_list_is = valid_axes.copy()
+    x_index = axes_list[xaxis]
+    y_index = axes_list[yaxis]
+    z_index = axes_list[zaxis]
+
+    if axes_idx_list_tobe[0] != axes_idx_list_is[0]:
+        axes_idx_list_is[0], axes_idx_list_is[axes_idx_list_tobe[0]] = axes_idx_list_is[axes_idx_list_tobe[0]], axes_idx_list_is[0]
+        panel = panel.swapaxes(0,axes_idx_list_tobe[0])
+    if axes_idx_list_tobe[1] != axes_idx_list_is[1]:
+        axes_idx_list_is[1], axes_idx_list_is[2] = axes_idx_list_is[2], axes_idx_list_is[1]
+        panel = panel.swapaxes(1,2)
+    if not np.array_equal(axes_idx_list_is,axes_idx_list_tobe):
+        txt = 'not possible'
+        raise ValueError(txt)
+
+    z = panel.values[:,:,sub_set].transpose()
+    x = np.repeat(np.array([x_index]), y_index.shape[0], axis = 0)
+    y = np.repeat(np.array([y_index]), x_index.shape[0], axis = 0).transpose()
+
+    f,a = plt.subplots()
+    pc = a.pcolormesh(x, y , z, **kwargs)
+
+
+    if 'datetime' in panel.items.dtype_str:
+        f.autofmt_xdate()
+    cb = f.colorbar(pc)
+    a.set_xlabel(panel.items.name)
+    a.set_ylabel(panel.major_axis.name)
+    cb.set_label(panel.minor_axis[sub_set])
+    return f,a,pc,cb
