227b98a882c1a99d3d3eb31ab71ac29f544cc612
hagne <hagen@hagne.net>
2016-02-08 12:50:14 -0700

    ArmDict class is no more

--- atmPy/data_archives/arm/_tools.py
+++ atmPy/data_archives/arm/_tools.py
@@ -7,23 +7,3 @@ def _get_time(file_obj):
     time = pd.to_datetime(0) + pd.to_timedelta(bt[:].flatten()[0], unit = 's') + pd.to_timedelta(toff[:], unit = 's')
     return time
 
-class ArmDict(dict):
-    def __init__(self, plottable = [], plot_kwargs = {}, *args):
-        super(ArmDict,self).__init__(self,*args)
-        self.plottable = plottable
-        self.plot_kwargs = plot_kwargs
-
-    def plot(self, which = 'all', fig_size = None):
-        if which == 'all':
-            for item in self.plottable:
-                out = self[item].plot(**self.plot_kwargs)
-                if type(out).__name__ == 'AxesSubplot':
-                    a = out
-                elif len(out) == 4:
-                    a = out[1]
-
-                a.set_title(item)
-                if fig_size:
-                    f = a.get_figure()
-                    f.set_size_inches((fig_size))
-                # return out
