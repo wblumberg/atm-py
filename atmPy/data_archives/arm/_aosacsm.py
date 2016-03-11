537c81700c2a4e5eee9b9b4bae33d0e49f85e22a
hagne <hagen@hagne.net>
2016-03-11 12:41:55 -0700

    new: decorators juchuuuu

--- atmPy/data_archives/arm/_aosacsm.py
+++ atmPy/data_archives/arm/_aosacsm.py
@@ -1,76 +1,16 @@
-import pandas as pd
-from atmPy.general import timeseries
-from atmPy.aerosols.instruments.AMS import AMS
-from atmPy.data_archives.arm._netCDF import ArmDataset
-
-class ArmDatasetSub(ArmDataset):
-    def __init__(self,*args, **kwargs):
-        super(ArmDatasetSub,self).__init__(*args, **kwargs)
-        self.__kappa = None
-        self.__mass_concentration_corr = None
-        self.__refractive_index = None
-
-    def _parse_netCDF(self):
-        super(ArmDatasetSub,self)._parse_netCDF()
-        mass_concentrations = pd.DataFrame(index = self.time_stamps)
-        mass_conc_keys = ['total_organics','ammonium','sulfate','nitrate','chloride']
-
-        for k in mass_conc_keys:
-            mass_concentrations[k] = pd.Series(self._read_variable(k), index = self.time_stamps)
-
-        mass_concentrations.columns.name = 'Mass conc. ug/m^3'
-        mass_concentrations.index.name = 'Time'
-
-        # for v in file_obj.variables.keys():
-        #     var = file_obj.variables[v]
-        #     print(v)
-        #     print(var.long_name)
-        #     print(var.shape)
-        #     print('--------')
-
-        org_mx = self._read_variable('org_mx')
-        org_mx = pd.DataFrame(org_mx, index = self.time_stamps)
-        org_mx.columns = self._read_variable('amus')
-        org_mx.columns.name = 'amus (m/z)'
-
-        # out = _tools.ArmDict(plottable = ['mass_concentrations', 'Organic mass spectral matrix'])
-        # out['mass_concentrations'] = timeseries.TimeSeries(mass_concentrations)
-        # out['Organic mass spectral matrix'] = timeseries.TimeSeries_2D(org_mx)
-
-        self.mass_concentrations = AMS.AMS_Timeseries_lev01(mass_concentrations)
-        self.mass_concentrations.data['total'] = self.mass_concentrations.data.sum(axis = 1)
-        self.organic_mass_spectral_matrix = timeseries.TimeSeries_2D(org_mx)
-        return
-
-    @property
-    def mass_concentration_corr(self):
-        if self.__mass_concentration_corr is None:
-            self.__mass_concentration_corr = self.mass_concentrations.calculate_electrolyte_mass_concentrations()
-            self.__mass_concentration_corr.data['total'] = self.__mass_concentration_corr.data.sum(axis = 1)
-        return self.__mass_concentration_corr
-
-    @property
-    def kappa(self):
-        if self.__kappa is None:
-            self.__kappa = self.mass_concentration_corr.calculate_kappa()
-        return self.__kappa
-
-    @property
-    def refractive_index(self):
-        if self.__refractive_index is None:
-            self.__refractive_index = self.mass_concentration_corr.calculate_refractive_index()
-        return self.__refractive_index
-
-    def plot_all(self):
-        self.mass_concentrations.plot()
-        self.organic_mass_spectral_matrix.plot()
+import pandas as _pd
+from atmPy.general import timeseries as _timeseries
+from atmPy.aerosols.instruments.AMS import AMS as _AMS
+from atmPy.data_archives.arm._netCDF import ArmDataset as _ArmDataset
+from atmPy.tools import decorators as _decorators
 
 def _concat_rules(arm_data_objs):
+    """nothing here"""
     # out = arm_data_obj
     out = ArmDatasetSub(False)
-    out.mass_concentrations = AMS.AMS_Timeseries_lev01(
-        pd.concat([i.mass_concentrations.data for i in arm_data_objs]))
-    out.organic_mass_spectral_matrix = timeseries.TimeSeries_2D(pd.concat([i.organic_mass_spectral_matrix.data for i in arm_data_objs]))
+    out.mass_concentrations = _AMS.AMS_Timeseries_lev01(
+        _pd.concat([i.mass_concentrations.data for i in arm_data_objs]))
+    out.organic_mass_spectral_matrix = _timeseries.TimeSeries_2D(_pd.concat([i.organic_mass_spectral_matrix.data for i in arm_data_objs]))
 
     # out = _tools.ArmDict(plottable = ['mass_concentrations', 'Organic mass spectral matrix'])
     # out['mass_concentrations'] = timeseries.TimeSeries(pd.concat([i['mass_concentrations'].data for i in files]))
@@ -90,3 +30,74 @@ def _concat_rules(arm_data_objs):
         # data_qc[data_qc <= quality_control] = 0
         # data_qc[data_qc > quality_control] = 1
         # mass_concentrations[k] = pd.Series(data, index = index)
+
+class ArmDatasetSub(_ArmDataset):
+    def __init__(self,*args, **kwargs):
+        super(ArmDatasetSub,self).__init__(*args, **kwargs)
+        self.__kappa = None
+        self.__mass_concentration_corr = None
+        self.__refractive_index = None
+
+    def _parse_netCDF(self):
+        super(ArmDatasetSub,self)._parse_netCDF()
+        mass_concentrations = _pd.DataFrame(index = self.time_stamps)
+        mass_conc_keys = ['total_organics','ammonium','sulfate','nitrate','chloride']
+
+        for k in mass_conc_keys:
+            mass_concentrations[k] = _pd.Series(self._read_variable(k), index = self.time_stamps)
+
+        mass_concentrations.columns.name = 'Mass conc. ug/m^3'
+        mass_concentrations.index.name = 'Time'
+
+        # for v in file_obj.variables.keys():
+        #     var = file_obj.variables[v]
+        #     print(v)
+        #     print(var.long_name)
+        #     print(var.shape)
+        #     print('--------')
+
+        org_mx = self._read_variable('org_mx')
+        org_mx = _pd.DataFrame(org_mx, index = self.time_stamps)
+        org_mx.columns = self._read_variable('amus')
+        org_mx.columns.name = 'amus (m/z)'
+
+        # out = _tools.ArmDict(plottable = ['mass_concentrations', 'Organic mass spectral matrix'])
+        # out['mass_concentrations'] = timeseries.TimeSeries(mass_concentrations)
+        # out['Organic mass spectral matrix'] = timeseries.TimeSeries_2D(org_mx)
+
+        self.mass_concentrations = _AMS.AMS_Timeseries_lev01(mass_concentrations)
+        self.mass_concentrations.data['total'] = self.mass_concentrations.data.sum(axis = 1)
+        self.organic_mass_spectral_matrix = _timeseries.TimeSeries_2D(org_mx)
+        return
+
+    @property
+    def mass_concentration_corr(self):
+        if self.__mass_concentration_corr is None:
+            self.__mass_concentration_corr = self.mass_concentrations.calculate_electrolyte_mass_concentrations()
+            self.__mass_concentration_corr.data['total'] = self.__mass_concentration_corr.data.sum(axis = 1)
+        return self.__mass_concentration_corr
+
+    @property
+    def kappa(self):
+        if self.__kappa is None:
+            self.__kappa = self.mass_concentration_corr.calculate_kappa()
+        return self.__kappa
+
+    @property
+    @_decorators.change_doc(_AMS.AMS_Timeseries_lev02.calculate_refractive_index, add_warning=False)
+    def refractive_index(self):
+        if self.__refractive_index is None:
+            self.__refractive_index = self.mass_concentration_corr.calculate_refractive_index()
+        return self.__refractive_index
+
+    # @property
+    # def refractive_index(self):
+    #     if self.__refractive_index is None:
+    #         self.__refractive_index = self.mass_concentration_corr.calculate_refractive_index()
+    #     return self.__refractive_index
+
+
+    def plot_all(self):
+        self.mass_concentrations.plot()
+        self.organic_mass_spectral_matrix.plot()
+
---
+++ atmPy/tools/decorators.py
@@ -0,0 +1,31 @@
+
+
+def change_doc(other_func, add_warning = True, keep_new_doc = True):
+    '''This decorator changes the docstring to that of another function
+    Arguments
+    ---------
+    add_warning: bool
+        if a warning is printed
+    keep_new_doc: bool
+        If the newly defined docstring is added to the changed one
+    '''
+
+    doc_other = other_func.__doc__
+    def wrapper(func):
+        def inner(*args, **kwargs):
+            wrapped_func = func(*args, **kwargs)
+#             func.__doc__ = other_func.__doc__
+            return wrapped_func
+        if keep_new_doc and func.__doc__:
+            note = 'Note from wrapper:\n'
+            wrapper_doc = func.__doc__
+            note_II = 'Original doc string:\n'
+            doc = note + wrapper_doc + '\n\n' + note_II
+        else:
+            doc = ''
+        if add_warning:
+            txt = 'This is the docstring of a function that has been decorated/composed. Function might behave wired.\n\n'
+            doc = txt + doc
+        inner.__doc__ = doc + doc_other
+        return inner
+    return wrapper
