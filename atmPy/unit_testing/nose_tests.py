a1e61bb8ec67147ef968c5d9132463e4770b4585
hagne <hagen@hagne.net>
2016-12-20 19:40:38 -0700

    fixed the testing and installation

--- atmPy/unit_testing/nose_tests.py
+++ atmPy/unit_testing/nose_tests.py
@@ -18,17 +18,19 @@ class ArmDataTests(TestCase):
         soll = pd.read_csv(test_data_folder + '1twr10xC1_rh.csv', index_col=0,
                            dtype={'rh_25m': np.float32, 'rh_60m': np.float32}
                            )
+        soll.columns.name = out.relative_humidity.data.columns.name
         self.assertTrue(np.all(out.relative_humidity.data == soll))
 
         # temp
         soll = pd.read_csv(test_data_folder + '1twr10xC1_temp.csv', index_col=0,
                            dtype={'temp_25m': np.float32, 'temp_60m': np.float32}
                            )
+        soll.columns.name = out.temperature.data.columns.name
         self.assertTrue(np.all(out.temperature.data == soll))
 
         # vapor pressure
         soll = pd.read_csv(test_data_folder + '1twr10xC1_p_vapor.csv', index_col=0,
                            dtype={'vap_pres_25m': np.float32, 'vap_pres_60m': np.float32}
                            )
-
+        soll.columns.name = out.vapor_pressure.data.columns.name
         self.assertTrue(np.all(out.vapor_pressure.data == soll))
