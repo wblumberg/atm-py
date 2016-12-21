6e34951f21e80198f38a44f75a7d40cee46c006f
hagne <hagen@hagne.net>
2016-12-20 18:40:58 -0700

    fixed the testing and installation

--- atmPy/unit_testing/nose_tests.py
+++ atmPy/unit_testing/nose_tests.py
@@ -1,29 +1,34 @@
+from unittest import TestCase
 import numpy as np
 import pandas as pd
-test_data_folder = './test_data/'
+import os
+test_data_folder = os.path.join(os.path.dirname(__file__), 'test_data/')
+# print(test_data_folder)
+# test_data_folder = './test_data/'
 #### data archives
 ######## ARM
 from atmPy.data_archives.arm import _read_data
 
-def test_1twr10xC1():
-    out = _read_data.read_cdf(test_data_folder, data_product='1twr10xC1')
-    out = out['1twr10xC1']
+class ArmDataTests(TestCase):
+    def test_1twr10xC1(self):
+        out = _read_data.read_cdf(test_data_folder, data_product='1twr10xC1')
+        out = out['1twr10xC1']
 
-    # rh
-    soll = pd.read_csv(test_data_folder + '1twr10xC1_rh.csv', index_col=0,
-                       dtype={'rh_25m': np.float32, 'rh_60m': np.float32}
-                       )
-    assert np.all(out.relative_humidity.data == soll)
+        # rh
+        soll = pd.read_csv(test_data_folder + '1twr10xC1_rh.csv', index_col=0,
+                           dtype={'rh_25m': np.float32, 'rh_60m': np.float32}
+                           )
+        self.assertTrue(np.all(out.relative_humidity.data == soll))
 
-    # temp
-    soll = pd.read_csv(test_data_folder + '1twr10xC1_temp.csv', index_col=0,
-                       dtype={'temp_25m': np.float32, 'temp_60m': np.float32}
-                       )
-    assert np.all(out.temperature.data == soll)
+        # temp
+        soll = pd.read_csv(test_data_folder + '1twr10xC1_temp.csv', index_col=0,
+                           dtype={'temp_25m': np.float32, 'temp_60m': np.float32}
+                           )
+        self.assertTrue(np.all(out.temperature.data == soll))
 
-    # vapor pressure
-    soll = pd.read_csv(test_data_folder + '1twr10xC1_p_vapor.csv', index_col=0,
-                       dtype={'vap_pres_25m': np.float32, 'vap_pres_60m': np.float32}
-                       )
+        # vapor pressure
+        soll = pd.read_csv(test_data_folder + '1twr10xC1_p_vapor.csv', index_col=0,
+                           dtype={'vap_pres_25m': np.float32, 'vap_pres_60m': np.float32}
+                           )
 
-    assert np.all(out.vapor_pressure.data == soll)
+        self.assertTrue(np.all(out.vapor_pressure.data == soll))
--- setup.py
+++ setup.py
@@ -1,9 +1,25 @@
-from distutils.core import setup
+import sys
 
-setup(name='atmPy',
-      version='0.1',
-      description='Python Distribution Utilities',
-      author='Hagen Telg and Matt Richardson',
-      author_email='matt.richardson@msrconsults.com',
-      packages=['atmPy'], requires=['numpy', 'pandas', 'matplotlib', 'scipy']
-      )
+required_verion = (3,)
+if sys.version_info < required_verion:
+    raise ValueError('atm-py needs at least python {}! You are trying to install it under python {}'.format('.'.join(str(i) for i in required_verion), sys.version))
+
+import ez_setup
+ez_setup.use_setuptools()
+
+from setuptools import setup, find_packages
+setup(
+    name="atm-py",
+    version="0.1",
+    packages=find_packages(),
+    author="Hagen Telg",
+    author_email="hagen@hagnet.net",
+    description="This package contains atmospheric science tools",
+    license="MIT",
+    keywords="atmospheric science tools",
+    url="https://github.com/hagne/atm-py",
+    install_requires=['numpy','pandas', 'scipy'],
+    extras_require={'plotting': ['matplotlib'],},
+    test_suite='nose.collector',
+    tests_require=['nose'],
+)
