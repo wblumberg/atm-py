ac87ed3b6f34634acd904049ff517aa158b54499
hagne <hagen@hagne.net>
2018-08-16 11:19:28 -0600

    minor restructuring

--- atmPy/data_archives/aeronet/aeronet.py
+++ atmPy/data_archives/aeronet/aeronet.py
@@ -3,6 +3,8 @@ from atmPy.general import timeseries as _timeseries
 import numpy as _np
 import os as _os
 import pandas as _pd
+from atmPy.aerosols.physics import column_optical_properties as _column_optical_properties
+
 
 def _read_header(folder, fname):
     """Read the header of file in folder and reterns a dict with relevant data"""
@@ -50,15 +52,15 @@ def _header_tests(folder, fname, version='3', level='2.0', data_product='AOD', s
     return _np.all(_np.array([isversion, islevel, isproduct, issite]))
 
 
-def _path2files(files, site, window, perform_header_test, verbose):
+def _path2files(path, site, window, perform_header_test, verbose):
     # folder or single file .... or list
-    if _os.path.isdir(files):
-        folder = files
+    if _os.path.isdir(path):
+        folder = path
         files = _os.listdir(folder)
         if verbose:
             print('{} files in folder'.format(len(files)))
-    elif _os.path.isfile(files):
-        folder, file = _os.path.split(files)
+    elif _os.path.isfile(path):
+        folder, file = _os.path.split(path)
         files = [file]
     else:
         raise ValueError('currently only folder and single files are allowed for the files argument')
@@ -123,7 +125,7 @@ def _read_files(folder, files, verbose):
     return data
 
 
-class Aeronet_AOD(object):
+class Aeronet_AOD(_column_optical_properties.AOD_AOT):
     pass
 
 
@@ -131,7 +133,7 @@ def open_path(path='/Volumes/HTelg_4TB_Backup/AERONET/',
               site='BONDVILLE',
               window=('2017-01-01', '2017-02-01'),
               perform_header_test=True,
-              fill_gaps=True,
+              fill_gaps=False,
               verbose=False,):
 
     files, folder = _path2files(path, site, window, perform_header_test, verbose)
@@ -145,21 +147,8 @@ def open_path(path='/Volumes/HTelg_4TB_Backup/AERONET/',
         if verbose:
             print('done')
 
-    # generate Aeronet_AOD and add AOD to class
-    aaod = Aeronet_AOD()
-
-    ## select columns that show AOD
-    aodcols = [col for col in data.data.columns if (col[:3] == 'AOD' and 'Empty' not in col)]
-    data_aod = data._del_all_columns_but(aodcols)
-    newcol = _np.array([c.replace('AOD_', '').replace('nm', '') for c in aodcols]).astype(int)
-
-    ## rename columns
-    data_aod.data.rename(columns=dict(zip(aodcols, newcol)), inplace=True)
-    data_aod.data.columns.name = 'AOD@wavelength(nm)'
-    data_aod.data.dropna(axis=1, how='all', inplace=True)
-
-    ## add the resulting Timeseries to the class
-    aaod.AOD = data_aod
+    if window:
+        data = data.zoom_time(window[0], window[1])
 
     # add Site class to Aeronet_AOD class
     lon = data.data['Site_Longitude(Degrees)'].dropna().iloc[0]
@@ -167,6 +156,21 @@ def open_path(path='/Volumes/HTelg_4TB_Backup/AERONET/',
     alt = data.data['Site_Elevation(m)'].dropna().iloc[0]
     site_name = data.data['AERONET_Site_Name'].dropna().iloc[0]
 
-    aaod.site = _measurement_site.Site(lat, lon, alt, name=site_name)
+    # aaod.site = _measurement_site.Site(lat, lon, alt, name=site_name)
+    # generate Aeronet_AOD and add AOD to class
+    aaot = Aeronet_AOD(lat, lon, elevation = alt, name = site_name, name_short = None, timezone = 0)
 
-    return aaod
+    ## select columns that show AOD
+    aodcols = [col for col in data.data.columns if (col[:3] == 'AOD' and 'Empty' not in col)]
+    data_aot = data._del_all_columns_but(aodcols)
+    newcol = _np.array([c.replace('AOD_', '').replace('nm', '') for c in aodcols]).astype(int)
+
+    ## rename columns
+    data_aot.data.rename(columns=dict(zip(aodcols, newcol)), inplace=True)
+    data_aot.data.columns.name = 'AOD@wavelength(nm)'
+    data_aot.data.dropna(axis=1, how='all', inplace=True)
+
+    ## add the resulting Timeseries to the class
+    aaot.AOT = data_aot
+
+    return aaot
