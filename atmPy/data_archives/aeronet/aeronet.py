57d150784a4b65c00efe7d6cb8ad7de0fadfa81e
hagne <hagen@hagne.net>
2018-09-17 14:18:02 -0600

    new: different version that are used at campains

--- atmPy/data_archives/aeronet/aeronet.py
+++ atmPy/data_archives/aeronet/aeronet.py
@@ -13,45 +13,77 @@ def _read_header(folder, fname):
 
     out = {}
     # version
-    out['version'] = head[0].replace(';', '').split()[-1]
+    if head[0].split('<p>')[0] == 'Level 2.0. Quality Assured Data.':
+        out['version'] = head[1].split()[1]
+    else:
+        out['version'] = head[0].replace(';', '').split()[-1]
+
     if out['version'] == '3':
         out['header_size'] = 6
+        # level
+        out['level'] = head[2].split(':')[1].split()[-1]
+        # product
+        out['product'] = head[2].split(':')[1].split()[0]
+        # site
+        out['site'] = head[1].strip()
+
+    elif out['version'] == '2':
+        out['header_size'] = 4
+        out['level'] = head[0].split('.')[0].split()[1]
+        out['product'] = head[3].split()[0]
+        dt = dict([i.split('=') for i in head[2].split(',')])
+        out['site'] = dt['Location']
+        out['lat'] = float(dt['lat'])
+        out['lon'] = float(dt['long'])
+        out['elevation'] = float(dt['elev'])
+
     else:
         raise ValueError('Version {} unknown. Programming required')
 
-    # level
-    out['level'] = head[2].split(':')[1].split()[-1]
-    # product
-    out['product'] = head[2].split(':')[1].split()[0]
-    # site
-    out['site'] = head[1].strip()
-
     return out
 
+class HeaderError(Exception):
+    pass
 
-def _header_tests(folder, fname, version='3', level='2.0', data_product='AOD', site='BONDVILLE', raise_error=True):
-    """This tests if the file is actually holding what it promisses"""
-    with open(folder + '/' + fname) as myfile:
-        head = [next(myfile) for x in range(6)]
+def _header_tests(folder, fname, version='3', level='2.0', data_product='AOD', site='BONDVILLE'):
+    head = _read_header(folder, fname)
+    if head['version'] != version:
+        raise HeaderError('Version indicated in file header ({}) does not match requried version ({})'.format(head['version'], version))
+    if head['level'] != level:
+        raise HeaderError('Data level indicated in file header ({}) does not match requried level ({})'.format(head['level'], level))
+    if head['product'] != data_product:
+        raise HeaderError('Data product indicated in file header ({}) does not match requried data product ({})'.format(head['product'], data_product))
+    if head['site'] != site:
+        raise HeaderError( 'Measurement site indicated in file header ({}) does not match requried site ({})'.format(head['site'], site))
+    if head['site'] != site:
+        raise HeaderError( 'Measurement site indicated in file header ({}) does not match requried site ({})'.format(head['site'], site))
+    return True
 
-    # version correct?
-    isversion = head[0].replace(';', '').split()[-1] == version
-    # level correct ?
-    islevel = head[2].split(':')[1].split()[-1] == level
-    # product correct?
-    isproduct = head[2].split(':')[1].split()[0] == data_product
-    # site correct?
-    site_from_fname = head[1].strip()
-    issite = site_from_fname == site
 
-    if raise_error:
-        assert (isversion)
-        assert (islevel)
-        assert (isproduct)
-        if not issite:
-            raise KeyError('{} does not macht the site requirement of {}'.format(site_from_fname, site))
 
-    return _np.all(_np.array([isversion, islevel, isproduct, issite]))
+# def _header_tests(folder, fname, version='3', level='2.0', data_product='AOD', site='BONDVILLE', raise_error=True):
+#     """This tests if the file is actually holding what it promisses"""
+#     with open(folder + '/' + fname) as myfile:
+#         head = [next(myfile) for x in range(6)]
+#
+#     # version correct?
+#     isversion = head[0].replace(';', '').split()[-1] == version
+#     # level correct ?
+#     islevel = head[2].split(':')[1].split()[-1] == level
+#     # product correct?
+#     isproduct = head[2].split(':')[1].split()[0] == data_product
+#     # site correct?
+#     site_from_fname = head[1].strip()
+#     issite = site_from_fname == site
+#
+#     if raise_error:
+#         assert (isversion)
+#         assert (islevel)
+#         assert (isproduct)
+#         if not issite:
+#             raise KeyError('{} does not macht the site requirement of {}'.format(site_from_fname, site))
+#
+#     return _np.all(_np.array([isversion, islevel, isproduct, issite]))
 
 
 def _path2files(path, site, window, perform_header_test, verbose):
@@ -122,6 +154,7 @@ def _read_files(folder, files, verbose):
     data = _pd.concat(data_list)
     data[data == -999.0] = _np.nan
     data = _timeseries.TimeSeries(data, sampling_period=15 * 60)
+    data.header = header_first
     if verbose:
         print('done')
     return data
@@ -132,15 +165,18 @@ class Aeronet_AOD(_column_optical_properties.AOD_AOT):
 
 
 def open_path(path='/Volumes/HTelg_4TB_Backup/AERONET/',
-              site='BONDVILLE',
-              window=('2017-01-01', '2017-02-01'),
-              perform_header_test=True,
+              site= None, #'BONDVILLE',
+              window= None, #('2017-01-01', '2017-02-01'),
+              perform_header_test=False,
               fill_gaps=False,
-              verbose=False,):
+              verbose=False,
+              keep_original_data = False):
 
     files, folder = _path2files(path, site, window, perform_header_test, verbose)
     #     files = clean_up_files(files, None, None, None, verbose)
     data = _read_files(folder, files, verbose)
+    if keep_original_data:
+        data_orig = data.copy()
 
     if fill_gaps:
         if verbose:
@@ -154,19 +190,35 @@ def open_path(path='/Volumes/HTelg_4TB_Backup/AERONET/',
     if data.data.shape[0] == 0:
         raise ValueError('There is no data in the selected time window.')
     # add Site class to Aeronet_AOD class
-    lon = data.data['Site_Longitude(Degrees)'].dropna().iloc[0]
-    lat = data.data['Site_Latitude(Degrees)'].dropna().iloc[0]
-    alt = data.data['Site_Elevation(m)'].dropna().iloc[0]
-    site_name = data.data['AERONET_Site_Name'].dropna().iloc[0]
+    if data.header['version'] == '3':
+        lon = data.data['Site_Longitude(Degrees)'].dropna().iloc[0]
+        lat = data.data['Site_Latitude(Degrees)'].dropna().iloc[0]
+        alt = data.data['Site_Elevation(m)'].dropna().iloc[0]
+        site_name = data.data['AERONET_Site_Name'].dropna().iloc[0]
+        ## select columns that show AOD
+        aodcols = [col for col in data.data.columns if (col[:3] == 'AOD' and 'Empty' not in col)]
+        data_aot = data._del_all_columns_but(aodcols)
+        newcol = _np.array([c.replace('AOD_', '').replace('nm', '') for c in aodcols]).astype(int)
+
+    elif data.header['version'] == '2':
+        lon = data.header['lon']
+        lat = data.header['lat']
+        alt = data.header['elevation']
+        site_name = data.header['site']
+        ## select columns that show AOD
+        aodcols = [col for col in data.data.columns if (col[:3] == 'AOT')]
+        data_aot = data._del_all_columns_but(aodcols)
+        newcol = _np.array([c.replace('AOT_', '') for c in aodcols]).astype(int)
+    else:
+        raise HeaderError('version {} not defined'.format(data.header['version']))
+
 
     # aaod.site = _measurement_site.Site(lat, lon, alt, name=site_name)
     # generate Aeronet_AOD and add AOD to class
     aaot = Aeronet_AOD(lat, lon, elevation = alt, name = site_name, name_short = None, timezone = 0)
+    if keep_original_data:
+        aaot.original_data = data_orig
 
-    ## select columns that show AOD
-    aodcols = [col for col in data.data.columns if (col[:3] == 'AOD' and 'Empty' not in col)]
-    data_aot = data._del_all_columns_but(aodcols)
-    newcol = _np.array([c.replace('AOD_', '').replace('nm', '') for c in aodcols]).astype(int)
 
     ## rename columns
     data_aot.data.rename(columns=dict(zip(aodcols, newcol)), inplace=True)
@@ -176,4 +228,6 @@ def open_path(path='/Volumes/HTelg_4TB_Backup/AERONET/',
     ## add the resulting Timeseries to the class
     aaot.AOD = data_aot
 
+    aaot.ang_exp = data._del_all_columns_but([col for col in data.data.columns if 'Angstrom' in col])
+    aaot.ang_exp.data.columns = [col.replace('Angstrom', '_Angstrom_Exponent') for col in aaot.ang_exp.data.columns]
     return aaot
