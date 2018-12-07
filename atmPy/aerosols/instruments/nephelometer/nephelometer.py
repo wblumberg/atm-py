697c1240dd4fe2a0fa05952529cf1214054d7336
hagne <hagen@hagne.net>
2018-12-07 10:37:34 -0700

    new: nephelometer class

--- atmPy/aerosols/instruments/nephelometer/nephelometer.py
+++ atmPy/aerosols/instruments/nephelometer/nephelometer.py
@@ -1,3 +1,7 @@
+from io import StringIO as _StringIO
+import pandas as _pd
+from atmPy.aerosols.instruments.nephelometer import data
+import matplotlib.pylab as _plt
 
 class Nephelometer(object):
     def __init__(self):
@@ -5,6 +9,7 @@ class Nephelometer(object):
         self._scattering_coeff = None
         self._hemisphericbackscatt_coeff = None
         self._RH_in_instrument = None
+        self.channels = Channels()
 
 
 
@@ -36,3 +41,29 @@ class TandemNephelometer(object):
     def __init__(self):
         self._nephelometer_dry = None
         self._nephelometer_wet = None
+
+class Channels(object):
+    def __init__(self):
+        buff = _StringIO(data.data_450)
+        df_450 = _pd.read_csv(buff, sep='  ', index_col=1, names=['450'], engine='python')
+        df_450.sort_index(inplace=True)
+        self.channel_450 = df_450
+
+        buff = _StringIO(data.data_550)
+        df_550 = _pd.read_csv(buff, sep='  ', index_col=1, names=['550'], engine='python')
+        df_550.sort_index(inplace=True)
+        self.channel_550 = df_550
+
+        buff = _StringIO(data.data_700)
+        df_700 = _pd.read_csv(buff, sep='  ', index_col=1, names=['700'], engine='python')
+        df_700.sort_index(inplace=True)
+        self.channel_700 = df_700
+
+    def plot(self, ax = None):
+        if not ax:
+            f,a = _plt.subplots()
+        self.channel_450.plot(ax = a)
+        self.channel_550.plot(ax = a)
+        self.channel_700.plot(ax = a)
+
+
