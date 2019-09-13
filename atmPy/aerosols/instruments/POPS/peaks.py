93fb0f53e83e8a4ea31b5fc2e56f7922f695f216
wblumberg <wblumberg@ou.edu>
2019-09-13 14:13:12 -0400

    Added debug statements to peaks.py to try to figure out why data won't load

--- atmPy/aerosols/instruments/POPS/peaks.py
+++ atmPy/aerosols/instruments/POPS/peaks.py
@@ -644,8 +644,9 @@ class peaks(object):
     
         """
         notMasked = np.where(self.data.Masked == 0)
+        print(notMasked, self.data, self.data.shape)
         # too_big_condi = np.where(self.data.Masked == 2)
-
+        print(self.data.index.values[notMasked])
         unique = np.unique(self.data.index.values[notMasked])
         N = np.zeros((unique.shape[0],bins.shape[0]-1))
         too_big = np.zeros(unique.shape[0])
@@ -663,8 +664,9 @@ class peaks(object):
         N = N.astype(np.float)
         too_big = too_big.astype(np.float)
 
+        print(unique, unique[1:], unique[:-1], np.timedelta64(1, 's'))
         deltaT = (unique[1:]-unique[:-1]) / np.timedelta64(1,'s')
-
+        print(deltaT)
         deltaT_sl = np.append(deltaT[0],deltaT)
         deltaT = np.repeat(np.array([deltaT_sl]),bins.shape[0]-1, axis=0)
         N/=deltaT.transpose()
